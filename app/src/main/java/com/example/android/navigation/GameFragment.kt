9764d14dc4be6621586ade5c67f4554891cb9555
your@email.com <you@example.com>
2020-07-27 12:31:03 +0700

    3.3.3 Step 3: Replace fragment classes with NavDirection classes

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -102,13 +102,14 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         view.findNavController()
-                                .navigate(GameFragmentDirections.
-                                actionGameFragmentToGameWonFragment())
+                                .navigate(GameFragmentDirections
+                                        .actionGameFragmentToGameWonFragment())
                         // We've won!  Navigate to the gameWonFragment.
                     }
                 } else {
                     view.findNavController()
-                            .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
+                            .navigate(GameFragmentDirections
+                                    .actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     // Game over! A wrong answer sends us to the gameOverFragment.
                 }
             }
--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -33,12 +33,14 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
                 R.layout.fragment_title,container,false)
 
         binding.playButton.setOnClickListener{view : View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
+            view.findNavController()..navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())}
         setHasOptionsMenu(true)
         return binding.root
     }
